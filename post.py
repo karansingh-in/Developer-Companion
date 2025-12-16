1d3dfc60d027527c2ddac7adabd0da97a9d43071
Karan <singh.karangkp@gmail.com>
2025-12-16 11:15:53 +0530

    posts a random advice from the data database with hastags and post count

---
+++ post.py
@@ -0,0 +1,41 @@
+import tweepy, random, schedule, time
+from data import advices, hashtags
+from auth import(
+    consumer_key, consumer_secret, access_token, access_token_secret, bearer_token
+)
+
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+# POSTING SOMETHING ----
+# client.create_tweet(text='this is test 3')
+
+# RETWEETING A POST ----
+# client.retweet(1982342611764453882)
+
+# REPLYING TO A POST ----
+# client.create_tweet(
+#     text="Nice post!",
+#     in_reply_to_tweet_id=1982342611764453882
+# )
+
+count = int(1)
+def post_a_tweet():
+    global count
+    try:
+        print('im here')
+        client.create_tweet(text=f'Post {count}\n' + random.choice(advices) + '\n\n' + random.choice(hashtags))
+        count += 1
+        print("posted something")
+    except Exception as e:
+        print("Error:", e)
+
+# schedule.every().day.at("00:16:00").do(job)
+# schedule.every().monday.at(f'07:{random.randint(10, 59)}:17').do(post_a_tweet)
+# schedule.every(10).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(random.randint(5,25))
